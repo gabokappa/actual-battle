931c4f18da6573b0c9365512e8ef8d2b3af422b4
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-03 15:34:58 +0100

    Project directory setup with all gems

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+
+  get '/' do
+    'Are you here for the battle?'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
