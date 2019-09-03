62d0847cb1e0dbc485a20f758c38397c2a3a1ff5
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-03 16:52:12 +0100

    First user step of user story one addressed needs refactoring

--- app.rb
+++ app.rb
@@ -3,9 +3,15 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    'Testing infrastructure working!'
+     erb :index
+    #'Testing infrastructure working!'
   end
 
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
