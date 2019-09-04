72058a00d512a10aeb6a069e7eb120a9d5744922
Oliver Antcliff <antcliffoc@gmail.com>
2019-09-04 14:07:02 +0100

    add test helper, sign_in_and_play

--- spec/features/show_player_2_hit_points_spec.rb
+++ spec/features/show_player_2_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Show hit points for player 2' do
   scenario 'Home page player 2 has full points' do
-  visit('/')
-  fill_in :player_1_name, with: 'Dave'
-  fill_in :player_2_name, with: 'Mittens'
-  click_button 'Submit'
+  sign_in_and_play()
   expect(page).to have_content 'Mittens HP: 100'
   end
 end
