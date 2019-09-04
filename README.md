b98770292b164235ac025c33338db994811402f1
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-04 13:43:42 +0100

    POST/ redirect /GET pattern

---
+++ README.md
@@ -0,0 +1,1 @@
+Week 3 Battle project working with Ruby, Sinatra and Capybara feature testing. And Rspec obvs.
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
      erb :index
@@ -8,10 +9,18 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
+
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  #run! if app_file == $0
+  run! if __FILE__ == $PROGRAM_NAME
 end
